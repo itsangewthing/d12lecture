5e067714d0cf8697c1b262cd4b162ec042cd4938
partytaitai <partytaitai@MacBook-Pro-3.local>
2022-07-19 09:54:08 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/d12lecture/D12lectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d12lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D12lectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D12lectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/d12lecture/D12lectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d12lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D12lectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
