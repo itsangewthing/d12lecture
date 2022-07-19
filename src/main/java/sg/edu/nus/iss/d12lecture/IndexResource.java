801dd7f799148fa9084dde3698551309a217da2a
partytaitai <partytaitai@MacBook-Pro-3.local>
2022-07-19 11:00:12 +0800

    indexresource added

---
+++ src/main/java/sg/edu/nus/iss/d12lecture/IndexResource.java
@@ -0,0 +1,18 @@
+package sg.edu.nus.iss.d12lecture;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path= {"/indexresource", "/indexresource.html"})
+public class IndexResource {
+
+    @GetMapping(produces ={"text/html"})
+    public String index(Model model){
+        return "indexresource"; //must be in double quotes, variable must have the same name
+        
+        
+    }
+}
